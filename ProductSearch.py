d5625808298d2c16ed0b429ec565901b8d7b18fb
vtoch23 <121360509+vtoch23@users.noreply.github.com>
2023-02-23 11:15:00 +0000

    added user input

--- ProductSearch.py
+++ ProductSearch.py
@@ -1,15 +1,20 @@
-def search(products: list, criterion: callable):
+def search(products: list, criterion: callable, price):
     list = []
     for product in products:
-        if criterion(product):
+        if criterion(product, price):
             list.append(product)
     return list        
  
-def price_under_4_euros(product):
-    return product[1] < 4
+def price_under_4_euros(product, price):
+    return product[1] < price
  
  
 if __name__ == "__main__":
-    products = [("banana", 5.95, 12), ("apple", 3.95, 3), ("orange", 4.50, 2), ("watermelon", 4.95, 22), ("kale", 0.99, 1)]
-    for product in search(products, price_under_4_euros):
+    product1 = input("Please enter a fruit: ")
+    price1 = int(input("Please enter the fruit's price: "))
+    product2 = input("PLease enter another fruit: ")
+    price2 = int(input("Please enter the fruit's price: "))
+    price = int(input("Enter maximum price of products you'd like to find: "))
+    products = [(product1, price1), (product2, price2)]
+    for product in search(products, price_under_4_euros, price):
         print(product)
